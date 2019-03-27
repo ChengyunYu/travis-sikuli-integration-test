bd2454710132f101ca98073bf22acc8db84bb321
Cheng Yun Yu <yucyun@cn.ibm.com>
2019-03-27 16:00:54 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/astaxie/beego"
+
+type HomeController struct {
+	beego.Controller
+}
+
+//接受者
+func (homeController *HomeController) Get(){
+	homeController.Ctx.WriteString("travis build 03/27/2019 3:42\n")
+}
+
+func main() {
+	beego.Router("/", &HomeController{})
+	beego.Run()
+}
